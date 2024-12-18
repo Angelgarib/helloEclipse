b785a02800bbff2433486dce1b05b4fba4254a11
Ángel José García <anggariba@alu.edu.gva.es>
2024-12-18 10:45:16 +0100

    Primer commit: proyecto creado.

---
+++ src/Tutorial/helloEclipse.java
@@ -0,0 +1,14 @@
+package Tutorial;
+
+public class helloEclipse {
+
+	public helloEclipse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
